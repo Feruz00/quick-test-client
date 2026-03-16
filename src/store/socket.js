11dbcb53fa2025e1449378c7c0f8281e8d7b0304
feruz00 <feruzatamyradow@mail.ru>
2026-03-16 17:40:39 +0500

    change transport

--- src/store/socket.js
+++ src/store/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_API_URL, {
-  autoConnect: false, // important
   withCredentials: true,
   transports: ['websocket'],
 });
