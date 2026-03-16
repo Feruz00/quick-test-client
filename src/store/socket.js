4378f890623476d793dec7eccdbb208c931e8e37
feruz00 <feruzatamyradow@mail.ru>
2026-03-16 15:05:39 +0500

    added websocket to socket.io

--- src/store/socket.js
+++ src/store/socket.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false, // important
   withCredentials: true,
+  transports: ['websocket'],
 });
 
 export default socket;
